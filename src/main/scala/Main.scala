b2469cfc3db4ad227303f4de010adfa7ca8b9064
Matvii <matkit27@gmail.com>
2022-06-17 17:05:14 +0300

    Final commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,76 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+package nazva
+import java.{util => ju}
 
-def msg = "I was compiled by Scala 3."
+
+
+case class Bag[A] private[Bag] (val map: Map[A, Int]):
+  override def toString(): String =
+  {
+    this.map.toString();
+  } 
+
+
+object Bag:
+  private def run[A](result: Map[A, Int], xs: Seq[A]): Map[A, Int] = 
+          var temp: Map[A, Int] = result;
+          xs match
+            case Seq(a, b*) => 
+                val t: Option[Int] = result.get(a);
+                t match
+                  case None => 
+                    temp += (a -> 1);
+                  case _ =>
+                    temp -= a;
+                    temp += (a -> (t.get + 1));
+                    
+                run(temp, b);
+            case _ => result;
+
+  def apply[A](xs: A*): Bag[A] = 
+      val m : Map[A, Int] = run(Map.empty[A, Int], xs);
+      Bag[A](m);
+
+
+  def add[A](b: Bag[A], as: A*): Bag[A] =
+    val m : Map[A, Int] = run(b.map, as);
+    Bag[A](m);
+
+  def remove[A](b: Bag[A], as: A): Bag[A] =
+    var temp = b.map
+    val t: Option[Int] = temp.get(as);
+    t match
+      case None => b;
+      case _ =>
+        temp -= as;
+        if t.get > 1 then
+          temp += (as -> (t.get - 1));
+
+        Bag[A](temp);
+
+  def toSet[A](b: Bag[A]): Set[A] =
+    b.map.keySet;
+
+  
+  def toList[A](b: Bag[A]): List[A] =
+    def run[A](result: List[A], xs: Bag[A]): List[A] = 
+      if (xs == Bag()) then result;
+      else
+        run((xs.map.keySet.head)::result, Bag.remove(xs, xs.map.keySet.head));
+
+    run(List[A](), b); 
+
+
+@main def hello: Unit = 
+  var t = Bag('a', 'a', 'b', 'c', 'c', 'c', '2')
+  println(Bag.toSet(t));
+  println(Bag.toList(t));
+  println(t);
+  t = Bag.add(t, 'f');
+  println(t);
+  t = Bag.remove(t, 'f');
+  println(t);
+  val d = Bag();
+  println(d);
+
+  println(List(1,4,3,10,0).sorted)
+ 
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,39 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+import munit.ScalaCheckSuite
+import org.scalacheck._
+import org.scalacheck.Prop._
+import org.scalacheck.Arbitrary.arbitrary
+import nazva._
+import scala.util.Random
+
+
+class IntegerSuite extends ScalaCheckSuite {
+
+  given [A: Arbitrary]: Arbitrary[Bag[A]] =
+    Arbitrary(Gen.listOf(arbitrary[A]).map(Bag(_*)))
+
+  property("Addition and deletion is correct and commutative") {
+    forAll { (a: Bag[Int], i: Int) =>
+      (Bag.remove(Bag.add(a, i), i) == a) && (
+      if a.map.contains(i) then (Bag.add(Bag.remove(a, i), i) == a)
+      else true )
+    }
+  }
+
+  property("Empty bag is always empty") {
+    forAll { (a: Int) =>
+      Bag.remove(Bag(), a) == Bag() 
+    }
+  }
+
+  property("Correct toSet conversion") {
+    forAll { (s: Set[Int]) =>
+      Bag.toSet(Bag(s.toSeq*)) == s
+    }
+  }
+
+  property("Correct toList conversion") {
+    forAll { (s: List[Int]) =>
+      Bag.toList(Bag(s.toSeq*)).sorted == s.sorted
+    }
   }
 }
